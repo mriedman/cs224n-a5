b09a84b8e618759e4f7c5889123e3a7f1a148da3
mriedman <mriedman1@gmail.com>
2022-02-17 17:11:04 +0000

    part e eval

--- src/run.py
+++ src/run.py
@@ -133,7 +133,7 @@ elif args.function == 'finetune':
                       num_workers=4, ckpt_path=args.writing_params_path)
         trainer1 = trainer.Trainer(model1, finetune_dataset, None, tconf)
     else:
-        model1.load_state_dict(torch.load(args.reading_params_path)).to(device)
+        model1.load_state_dict(torch.load(args.reading_params_path))
         tconf = trainer.TrainerConfig(max_epochs=10, batch_size=256, learning_rate=6e-4,
                                       lr_decay=True, warmup_tokens=512 * 20,
                                       final_tokens=200 * len(pretrain_dataset) * block_size,
